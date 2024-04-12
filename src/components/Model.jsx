a41b04d2e71bfd845d9e174a1c1ea9706581dda0
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-12 17:48:36 +0530

    added model scene

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef, useState } from "react";
+import ModelView from "./ModelView";
+import { yellowImg } from "../utils";
 
 const Model = () => {
-  return <div>Model</div>;
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: 'iPhone 15 pro in Natural Titanium',
+    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+    img: yellowImg, 
+  })
+
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  const small = useRef(new THREE.Group());
+
+  useGSAP(() => {
+    gsap.to("#heading", {
+      y: 0,
+      opacity: 1,
+    });
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
+        </h1>
+        <div className="mt-5 flex flex-col items-center">
+          <div className="relative h-[75vh] w-full overflow-hidden md:h-[90vh]">
+            <ModelView />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Model;
-
-// 2:03:21
