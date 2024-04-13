c735d9f3a28a58fc0ad9af353e8ea2dbf560abe6
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-13 16:19:09 +0530

    config fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/apple-website/",
   plugins: [
     react(),
     sentryVitePlugin({
