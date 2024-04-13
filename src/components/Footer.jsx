a459ea9b86debac84b8e04ec1230df32e3774c95
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-13 18:09:41 +0530

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { footerLinks } from "../constants";
 
 const Footer = () => {
   return (
-    <footer className="py-5 sm:px-10 px-5">
+    <footer className="py-5 sm:px-10 px-5 overflow-auto">
       <div className="screen-max-width">
         <div>
           <p className="font-semibold text-gray text-xs">
